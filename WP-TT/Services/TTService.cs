cd97b5d583923e842830feb22f8f9ad75d54dfa3
dgmachado <douglassmachado@live.com>
2015-10-08 13:22:11 -0300

    Remote datetime

--- WP-TT/Services/TTService.cs
+++ WP-TT/Services/TTService.cs
@@ -10,20 +10,26 @@ namespace WP_TT.Services
 {
     internal class TTService
     {
-        public async Task<DateTime> RemoteDatetime(){
-            var httpClient = new HttpClient();
-            var result = await httpClient.GetAsync(new Uri("https://tt.ciandt.com/.net/index.ashx/GetClockDeviceInfo?deviceID=2"));
+        private DateTime ExtractDatetimeFromRemoteDatetimeHttpResponse(HttpResponseMessage result)
+        {
             var content = result.Content.ToString();
             var regex = new Regex(@"dtTimeEvent: new Date\((\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)\)");
             var match = regex.Match(content);
             var year = int.Parse(match.Groups[1].Value);
-            var month = int.Parse(match.Groups[2].Value);
+            var month = int.Parse(match.Groups[2].Value) + 1;
             var day = int.Parse(match.Groups[3].Value);
             var hour = int.Parse(match.Groups[4].Value);
             var minutes = int.Parse(match.Groups[5].Value);
             var seconds = int.Parse(match.Groups[6].Value);
 
             return new DateTime(year, month, day, hour, minutes, seconds);
+        }
+        
+        public async Task<DateTime> RemoteDatetime()
+        {
+            var httpClient = new HttpClient();
+            var result = await httpClient.GetAsync(new Uri("https://tt.ciandt.com/.net/index.ashx/GetClockDeviceInfo?deviceID=2"));
+            return ExtractDatetimeFromRemoteDatetimeHttpResponse(result);
         }
 
         public bool login(string login, string password)
