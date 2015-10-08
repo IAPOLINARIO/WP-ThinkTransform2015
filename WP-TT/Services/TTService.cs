6ae08d3e5b817a1a2a1a28732bc3f4308466468c
dgmachado <douglassmachado@live.com>
2015-10-08 15:23:20 -0300

    Call to checkin

--- WP-TT/Services/TTService.cs
+++ WP-TT/Services/TTService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -10,6 +14,18 @@ namespace WP_TT.Services
 {
     internal class TTService
     {
+        private const string BASE_URL = "https://tt.ciandt.com/.net/index.ashx/";
+        private Uri baseUri;
+
+        private const int CHECK_RECORDED = 1;
+        private const int INVALID_CREDENTIALS = 2;
+        private const int OFFLINE_BACKEND = 3;
+
+        internal TTService()
+        {
+            baseUri = new Uri(BASE_URL);
+        }
+
         private DateTime ExtractDatetimeFromRemoteDatetimeHttpResponse(HttpResponseMessage result)
         {
             var content = result.Content.ToString();
@@ -24,11 +40,11 @@ namespace WP_TT.Services
 
             return new DateTime(year, month, day, hour, minutes, seconds);
         }
-        
+
         public async Task<DateTime> RemoteDatetime()
         {
             var httpClient = new HttpClient();
-            var result = await httpClient.GetAsync(new Uri("https://tt.ciandt.com/.net/index.ashx/GetClockDeviceInfo?deviceID=2"));
+            var result = await httpClient.GetAsync(new Uri(baseUri, "GetClockDeviceInfo?deviceID=2"));
             return ExtractDatetimeFromRemoteDatetimeHttpResponse(result);
         }
 
@@ -37,10 +53,96 @@ namespace WP_TT.Services
             return true;
         }
 
-        public bool DoCheckIn(){
-        // https://tt.ciandt.com/.net/index.ashx/GetClockDeviceInfo?deviceID=2
-            return true;
+        public string FixJson(string script)
+        {
+            Regex regexFixJson = new Regex("(\\s*?{\\s*?|\\s*?,\\s*?)(['\"])?([a-zA-Z0-9_]+)(['\"])?:");
+
+            string fixedJson = regexFixJson.Replace(script, "$1\"$3\":");
+
+            return fixedJson;
         }
 
+        public async Task<bool> DoCheckIn(string userName, string password)
+        {
+            Regex regexSuccessCheckIn = new Regex(@"success:\s*true");
+            Uri checkInUri = new Uri(baseUri, "SaveTimmingEvent");
+            var httpClient = new HttpClient();
+            DateTime checkinDateTime = await RemoteDatetime();
+
+            try
+            {
+                Dictionary<string, string> contentParameters = new Dictionary<string, string>
+                {
+                    {"deviceID", "2"},
+                    {"eventType", "1"},
+                    {"userName", userName},
+                    {"password", password},
+                    {"cracha", ""},
+                    {"costCenter", ""},
+                    {"leave", ""},
+                    {"func", ""},
+                    {"cdiDispositivoAcesso", "2"},
+                    {"cdiDriverDispositivoAcesso", "10"},
+                    {"cdiTipoIdentificacaoAcesso", "7"},
+                    {"oplLiberarPETurmaRVirtual", "false"},
+                    {"cdiTipoUsoDispositivo", "1"},
+                    {"qtiTempoAcionamento", "0"},
+                    {"d1sEspecieAreaEvento", "Nenhuma"},
+                    {"d1sAreaEvento", "Nenhum"},
+                    {"d1sSubAreaEvento", "Nenhum(a)"},
+                    {"d1sEvento", "Nenhum"},
+                    {"oplLiberarFolhaRVirtual", "false"},
+                    {"oplLiberarCCustoRVirtual", "false"},
+                    {"qtiHorasFusoHorario", "0"},
+                    {"cosEnderecoIP", "127.0.0.1"},
+                    {"nuiPorta", "7069"},
+                    {"oplValidaSenhaRelogVirtual", "false"},
+                    {"useUserPwd", "true"},
+                    {"useCracha", "false"},
+                    {"dtTimeEvent", ""},
+                    {"oplLiberarFuncoesRVirtual", "false"},
+                    {"sessionID", "0"},
+                    {"selectedEmployee", "0"},
+                    {"selectedCandidate", "0"},
+                    {"selectedVacancy", "0"},
+                    {"dtFmt", "d/m/Y"},
+                    {"tmFmt", "H:i:s"},
+                    {"shTmFmt", "H:i"},
+                    {"dtTmFmt", "d/m/Y H:i:s"},
+                    {"language", "0"}
+                };
+
+
+                var content = new Windows.Web.Http.HttpFormUrlEncodedContent(contentParameters);
+
+                HttpResponseMessage response = await httpClient.PostAsync(checkInUri, content);
+
+                string responseContent = await response.Content.ReadAsStringAsync();
+                string responseJson = FixJson(responseContent);
+
+                var responseType = new { success = false, msg = new { type = 0, msg = "" } };
+
+                var responseObject = JsonConvert.DeserializeAnonymousType(responseJson, responseType);
+
+                bool result = false;
+
+                switch ((int)responseObject.msg.type)
+                {
+                    case CHECK_RECORDED:
+                        result = true;
+                        break;
+                    case INVALID_CREDENTIALS:
+                    case OFFLINE_BACKEND:
+                        result = false;
+                        break;
+                }
+
+                return result && responseObject.success;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
