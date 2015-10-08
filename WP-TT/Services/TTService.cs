3973aae87db530914436042dedf09b85bc3de7d4
dgmachado <douglassmachado@live.com>
2015-10-08 11:08:44 -0300

    TTService added

---
+++ WP-TT/Services/TTService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace WP_TT.Services
+{
+    internal class TTService
+    {
+        public async DateTime RemoteDatetime(){
+            //var httpClient = new HttpClient();
+            //var result = await httpClient.GetAsync(new Uri("https://tt.ciandt.com/.net/index.ashx/GetClockDeviceInfo?deviceID=2"));
+            //var content = result.Content.ToString();
+            //var regex = new Regex("dtTimeEvent: new Date\((\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)\)");
+            //var match = regex.Match();
+            //match.
+            return DateTime.Now;
+        }
+
+        public bool login(string login, string password)
+        {
+            return true;
+        }
+
+        public bool DoCheckIn(){
+        // https://tt.ciandt.com/.net/index.ashx/GetClockDeviceInfo?deviceID=2
+            return true;
+        }
+
+    }
+}
